21971c66c45af80f2f9eaaa6f749e693745f0d21
Kangonaut <120662571+Kangonaut@users.noreply.github.com>
2023-10-04 18:40:13 +0200

    configure logging for celery

--- aotd_backend/aotd_backend/celery.py
+++ aotd_backend/aotd_backend/celery.py
@@ -1,7 +1,24 @@
 import os
 from celery import Celery
+from celery.signals import setup_logging
 
+# set the default Django settings module for the celery application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'aotd_backend.settings')
+
+# create the celery application
 app = Celery('aotd_backend')
+
+# all celery-related configuration keys should have a CELERY_ prefix
 app.config_from_object('django.conf:settings', namespace='CELERY')
+
+
+@setup_logging.connect
+def config_loggers(*args, **kwargs):
+    from logging.config import dictConfig
+    from django.conf import settings
+
+    dictConfig(settings.LOGGING)
+
+
+# load task modules from all registered Django app configs
 app.autodiscover_tasks()
