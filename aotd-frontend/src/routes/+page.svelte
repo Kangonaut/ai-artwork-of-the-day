7fb4d86cf42ad016f10d4d3b908bb044f3220330
Kangonaut <120662571+Kangonaut@users.noreply.github.com>
2023-11-06 23:00:17 +0100

    redesign home page

--- aotd-frontend/src/routes/+page.svelte
+++ aotd-frontend/src/routes/+page.svelte
@@ -1,45 +1,61 @@
-<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
-
-<div class="container h-full mx-auto flex justify-center items-center">
-	<div class="space-y-10 text-center flex flex-col items-center">
-		<h2 class="h2">Welcome Humanoid!</h2>
-		<figure>
-			<section class="img-bg" />
-			<img src="/logo.png" alt="logo">
-		</figure>
-	</div>
+<div class="h-full background-wallpaper bg-cover">
+  <div class="container h-full mx-auto flex justify-center items-center">
+    <div class="space-y-10 text-center flex flex-col items-center">
+      <div class="card p-5 max-w-lg">
+        <h2 class="h2 mb-5">Welcome Humanoid!</h2>
+        <p class="text-lg">
+          It's the little things that bring us humans joy. AI Artwork of the Day
+          (AotD) is here to brighten up your day with a little artwork just for
+          you!
+        </p>
+      </div>
+      <div class="card p-5 max-w-2xl">
+        <h3 class="h3 mb-5">Some Links 😉</h3>
+        <div
+          class="logo-cloud grid-cols-1 lg:!grid-cols-4 gap-1 bg-transparent"
+        >
+          <a
+            class="logo-item bg-transparent p-10"
+            href="https://github.com/Kangonaut/ai-artwork-of-the-day"
+          >
+            <div>
+              <box-icon type="logo" size="md" name="github" color="white" />
+              <span>GitHub</span>
+            </div>
+          </a>
+          <a class="logo-item bg-transparent p-10" href="https://kit.svelte.dev/">
+            <div>
+              <box-icon type="solid" size="md" name="window-alt" color="white" />
+              <span>SvelteKit</span>
+            </div>
+          </a>
+          <a
+            class="logo-item bg-transparent p-10"
+            href="https://www.djangoproject.com/"
+          >
+            <div>
+              <box-icon size="md" name="server" color="white" />
+              <span>Django</span>
+            </div>
+          </a>
+		  <a
+            class="logo-item bg-transparent p-10"
+            href="https://openai.com/"
+          >
+            <div>
+              <box-icon size="md" name="bot" color="white" />
+              <span>OpenAI</span>
+            </div>
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 
-<style lang="postcss">
-	figure {
-		@apply flex relative flex-col;
-	}
-	figure img,
-	.img-bg {
-		@apply w-64 h-64 md:w-80 md:h-80;
-	}
-	.img-bg {
-		@apply absolute z-[-1] rounded-full blur-[50px] transition-all;
-		animation: pulse 5s cubic-bezier(0, 0, 0, 0.5) infinite,
-			glow 5s linear infinite;
-	}
-	@keyframes glow {
-		0% {
-			@apply bg-primary-400/50;
-		}
-		33% {
-			@apply bg-secondary-400/50;
-		}
-		66% {
-			@apply bg-tertiary-400/50;
-		}
-		100% {
-			@apply bg-primary-400/50;
-		}
-	}
-	@keyframes pulse {
-		50% {
-			transform: scale(1.5);
-		}
-	}
+<style lang="css">
+  .background-wallpaper {
+    background-image: url("/wallpaper/ai-creator-3.jpeg");
+    background-position: center;
+  }
 </style>
